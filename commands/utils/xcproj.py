022c3549559f96f7a916c50c9dc03e9080bec34e
Jakub Turek <jkbturek@gmail.com>
2019-03-08 14:06:40 +0100

    Refactor file listing

--- commands/utils/xcproj.py
+++ commands/utils/xcproj.py
@@ -13,21 +13,16 @@ def list_swift_files(project_path, target_name):
     (project_directory, _) = os.path.split(project_path)
     project = XcodeProject.load(os.path.join(project_path, 'project.pbxproj'))
     target = project.get_target_by_name(target_name)
-    files = []
 
-    for build_phase_id in target.buildPhases:
-        build_phase = project.objects[build_phase_id]
-        if build_phase.isa == 'PBXSourcesBuildPhase':
-            for build_file_id in build_phase.files:
-                build_file = project.objects[build_file_id]
-                file_ref = project.objects[build_file.fileRef]
-                files.append(__full_path(project, file_ref))
-
+    build_phases = (project.objects[p] for p in target.buildPhases if project.objects[p].isa == 'PBXSourcesBuildPhase')
+    build_files = (f for p in build_phases for f in p.files)
+    build_files = (project.objects[project.objects[f].fileRef] for f in build_files)
+    files = (__full_path(project, file) for file in build_files)
     return [os.path.join(project_directory, file_path) for file_path in files]
 
 
 def __full_path(project, file_ref):
-    folder = None
+    folder = ''
 
     if file_ref.sourceTree == '<group>':
         parent = __parent_group(project, file_ref)
@@ -36,8 +31,6 @@ def __full_path(project, file_ref):
             folder = __full_path(project, parent)
     elif file_ref.sourceTree == '<absolute>':
         folder = '/'
-
-    folder = folder if folder else ''
 
     if 'path' in file_ref:
         return os.path.join(folder, file_ref['path'])
