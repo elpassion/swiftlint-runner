e6a9ceca77588af00ba3a763a0afa5655c6a7512
Jakub Turek <jkbturek@gmail.com>
2019-03-11 16:18:15 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+import pathlib
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name='swiftlint-runner',
     version='0.1',
+    long_description=README,
+    long_description_content_type='text/markdown',
     packages=['commands', 'commands.utils'],
     url='https://github.com/elpassion/swiftlint-runner',
     license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
     author='Jakub Turek',
     author_email='jakub.turek@elpassion.pl',
-    description='Missing SwiftLint runner with ability to define config file per target'
+    description='Missing SwiftLint runner with ability to define config file per target',
+    entry_points={
+        "console_scripts": [
+            "slrunner=commands.lint:lint",
+        ]
+    },
+    install_requires=['click', 'pyyaml', 'mod-pbxproj']
 )
